6a0d29d9bad26e618f585e773aabde0d2d59585f
lemuelsousa <lemuel124.ldsp@gmail.com>
2024-03-16 10:29:18 -0300

    update color scheme settings

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -1,8 +1,13 @@
-return { 
+return {
   "shaunsingh/nord.nvim",
   config = function()
+
+    vim.g.nord_contrast = true
+    vim.g.nord_borders = true
+    vim.g.nord_cursorline_transparent	= true
+
     require("nord").set()
-    vim.cmd.colorscheme "nord" 
+    vim.cmd.colorscheme "nord"
   end
 }
 
